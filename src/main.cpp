073ab8e203621270d0dde58eb133fa061b498b04
munozyanez <munoz.yanez@gmail.com>
2017-05-15 19:35:20 +0200

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -82,7 +82,7 @@ int main()
 
     double T=0.01;
     int loops = 2/T;
-    double vel = 1;
+    double vel = 2;
     int jointNumber = 3;
 
     double lastJointPos = rightArm.GetJoint(jointNumber);
